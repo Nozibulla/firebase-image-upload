e8c49330548ce965affdbb0e196ac4fa80da7f3b
Bashar Nozibulla <nbashar01@gmail.com>
2018-12-24 13:12:10 +0600

    gitignore modified

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: 'AIzaSyCfb5j-BP1l_oJi-ru9AdsYTYq9CXyBs0I',
+  authDomain: 'headless-graphql.firebaseapp.com',
+  databaseURL: 'https://headless-graphql.firebaseio.com',
+  projectId: 'headless-graphql',
+  storageBucket: 'headless-graphql.appspot.com',
+  messagingSenderId: '678900724624',
+};
+export { config };
